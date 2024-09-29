8f8d65a77b5896d068e0c99f4ff1312d774a7e7d
karlo <kaes_21@alu.uabcs.mx>
2024-09-28 23:22:03 -0700

    midia section information finished

--- src/components/movieDetails/MidiaSection.vue
+++ src/components/movieDetails/MidiaSection.vue
@@ -2,7 +2,82 @@
 
     import { ref, onMounted } from 'vue';
 
+    const videos = ref([])
+    const images = ref([])
+    const posters = ref([])
+
+    const mostPopularVideo = ref('')
+    const mostPopularImage = ref({"vote_average": 0.0})
+    const mostPopularPoster = ref({"vote_average": 0.0})
+
     const midia_showing = ref(0)
+
+    const fetchVideos = async () => {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YTZiZDFhOTcyM2EzY2VhMDI2YTllMjUyMDQ2YjMxNiIsIm5iZiI6MTcyNzIyOTY5MC43OTM1OTQsInN1YiI6IjY2ZjJmNWYwYTk3ODgwMTQ4ZjNiOTBiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fZfgCHZz_ePmrG_w_m4-p4_wbnkBfoUD1N1DKRDDMh4");
+
+        const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
+        };
+
+        fetch("https://api.themoviedb.org/3/movie/957452/videos", requestOptions)
+        .then((response) => response.json())
+        .then((response) => {
+            for (let i = 0; i < 6; i++) {
+                videos.value.push(response.results[i])
+            }
+
+            mostPopularVideo.value = response.results[0].name 
+        })
+    }
+
+    const fetchImages = async () => {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YTZiZDFhOTcyM2EzY2VhMDI2YTllMjUyMDQ2YjMxNiIsIm5iZiI6MTcyNzIyOTY5MC43OTM1OTQsInN1YiI6IjY2ZjJmNWYwYTk3ODgwMTQ4ZjNiOTBiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fZfgCHZz_ePmrG_w_m4-p4_wbnkBfoUD1N1DKRDDMh4");
+
+        const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
+        };
+
+        fetch("https://api.themoviedb.org/3/movie/957452/images", requestOptions)
+        .then((response) => response.json())
+        .then((response) => {
+
+            let i = 0
+            response.backdrops.forEach(image => {
+                if (i < 6){
+                    images.value.push(image);
+                    i++;
+                }
+
+                if (image.vote_average > mostPopularImage.value.vote_average){
+                    mostPopularImage.value = image;
+                }
+            });
+
+            let j = 0;
+            response.posters.forEach(poster => {
+                if (j < 9){
+                    posters.value.push(poster);
+                    j++;
+                }
+
+                if (poster.vote_average > mostPopularPoster.value.vote_average){
+                    mostPopularPoster.value = poster;
+                }
+            });
+
+        })
+    }
+
+    onMounted(() => {
+        fetchImages();
+        fetchVideos();
+    })
 
 </script>
 
@@ -23,16 +98,30 @@
 
         <div class="media-content">
             <div class="more-popular" v-if="midia_showing == 0">
-                more-popular
+                <div>
+                    {{ mostPopularVideo }}
+                </div>
+                <div>
+                    <img :src="'https://image.tmdb.org/t/p/w500' + mostPopularImage.file_path" alt="">
+                </div>
+                <div>
+                    <img :src="'https://image.tmdb.org/t/p/w500' + mostPopularPoster.file_path" alt="">
+                </div>
             </div>
             <div class="videos" v-if="midia_showing == 1">
-                videos
+                <div v-for="video in videos">
+                    videos
+                </div>
             </div>
             <div class="bg-imgs" v-if="midia_showing == 2">
-                bg-imgs
+                <div v-for="image in images">
+                    <img :src="'https://image.tmdb.org/t/p/w500' + image.file_path" alt="">
+                </div>
             </div>
             <div class="posters" v-if="midia_showing == 3">
-                posters
+                <div v-for="poster in posters">
+                    <img :src="'https://image.tmdb.org/t/p/w500' + poster.file_path" alt="">
+                </div>
             </div>
         </div>
 
