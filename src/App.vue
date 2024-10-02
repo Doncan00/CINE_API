00a9558c3752fcc2c30230c55bf90b7632d06735
Doncan00 <90412543+Doncan00@users.noreply.github.com>
2024-10-01 23:45:07 -0600

    Esto sirve, solo falta ponerle bien las rutas

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-import SeriesInfo from './components/tvDetails/SeriesInfo.vue';
 
 </script>
 
 <template>
   <div>
-    <SeriesInfo/>
-    <!-- <RouterView></RouterView> -->
+    <RouterView></RouterView>
   </div>
 </template>
--- src/router/routerInstance.js
+++ src/router/routerInstance.js
@@ -2,6 +2,7 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 
 import movieHome from '@/components/movieHome.vue'
 import MovieDetails from '@/MovieDetails.vue'
+import SeriesInfo from '@/components/tvDetails/SeriesInfo.vue'
 import KeywordResults from '@/KeywordResults.vue'
 import { createWebHistory } from 'vue-router'
 
@@ -9,6 +10,8 @@ const routes = [
   { path: '/', component: movieHome },
   { path: '/movie-details/:id', component: MovieDetails },
   { path: '/keyword/:id', component: KeywordResults },
+  { path: '/series-details/:id', component: SeriesInfo },
+  
 ]
 
 const router = createRouter({
