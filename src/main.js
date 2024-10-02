595df895f46b8bf74c6b9a77eac9192f7e8f2863
karlo <kaes_21@alu.uabcs.mx>
2024-10-01 20:10:01 -0700

    router import

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import movieDetails from './movieDetails.vue'
 import KeywordResults from './KeywordResults.vue'
 import movieHome from './components/movieHome.vue'
+import router from './router/routerInstance'
 
 createApp(App)
     .use(router)
